3e1ad51e6d3940678492475fe5bcc34a9ce141dc
juanvieira <juanvieira1@yahoo.com>
2025-04-11 11:06:47 -0300

    core: update constants

--- shared/constants.py
+++ shared/constants.py
@@ -5,5 +5,6 @@ load_dotenv()
 
 AGENT_NAME = "development_tutor"
 DESCRIPTION = "Um agente instrutor/tutor para desenvolvedores."
-MODEL = os.getenv("MODEL", "learnlm-1.5-pro-experimental")
+TUTOR_MODEL = os.getenv("TUTOR_MODEL", "learnlm-1.5-pro-experimental")
+BASE_MODEL = os.getenv("BASE_MODEL", "gemini-2.0-flash-thinking-exp-01-21")
 WHL_FILE_NAME = os.getenv("ADK_WHL_FILE", "")
